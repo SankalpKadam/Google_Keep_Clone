384dceef14e436026fe2da9fcf33cce41ee9a132
Sankalp Kadam <sankalptp07@gmail.com>
2023-07-07 10:55:48 -0400

    Developed the header, footer and sample note

---
+++ keeper/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading(){
+    return (
+        <header className="heading">
+            <h1>Keeper</h1>
+        </header>
+    );
+}
+
+export default Heading;
