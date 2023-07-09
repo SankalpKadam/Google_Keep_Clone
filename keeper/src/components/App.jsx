fb3369b1152efd0d5b9e670329ab6d14a66acbf0
Sankalp Kadam <sankalptp07@gmail.com>
2023-07-09 07:19:22 -0400

    Implemented dynamic delete

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -10,16 +10,25 @@ function App() {
     const [notes,setNote]=React.useState([])
     function addNote(params) {
         setNote(prevNotes => {
-            const newNote={key:notes.length,...params}
+            const newNote={key:notes.length,id:notes.length,...params}
             return [...prevNotes,newNote]
         })
     }
+
+    function deleteNote(params) {
+        setNote(prevNotes=>{
+            return (prevNotes.filter((Note, index)=> {
+                return params !== index
+            }))
+        })
+    }
+
     return (
     <div>
         <Heading />
         <TakeNote onClick={addNote}/>
         {notes.map( (note) =>{
-             return <Note key = {note.key} title = {note.title} content = {note.content}/>
+            return <Note key = {note.key} id={note.id} title = {note.title} content = {note.content} onClick={deleteNote}/>
         })}
         <Footer />
     </div>
--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Note(props){
+    function delNote(event) {
+        console.log("delete");
+        props.onClick(props.id)
+        event.preventDefault();
+    }
     return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button>-</button>
+            <button onClick={delNote}>-</button>
         </div>
     );
 }
