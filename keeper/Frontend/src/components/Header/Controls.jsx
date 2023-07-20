44abb1cdb0e6e5fa5576808dfeca75b80a4c4da2
Sankalp Kadam <sankalptp07@gmail.com>
2023-07-20 14:10:33 -0400

    Started the implementation of redux

--- keeper/Frontend/src/components/Header/Controls.jsx
+++ keeper/Frontend/src/components/Header/Controls.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {GoogleLogin, GoogleOAuthProvider} from "@react-oauth/google";
 
 function Controls() {
+    const client_id="617576154690-a7bou4fkn5snlel8qsfbmr6tk6j5prvv.apps.googleusercontent.com";
     return(
         <GoogleOAuthProvider clientId="str">
             <GoogleLogin 
